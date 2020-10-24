bdb53afba90e48f3b3e00a4395bb069cc55bab7f
PaulinaPacyna <62997314+PaulinaPacyna@users.noreply.github.com>
2020-10-24 22:51:29 +0200

    Article.py refactor

--- src/Article.py
+++ src/Article.py
@@ -9,16 +9,19 @@ Created on Sat Oct 24 19:57:22 2020
 from nltk.corpus import stopwords
 from nltk import FreqDist
 
+def txt_to_tokens(text):
+    #splitting text and filtering words containing only letters
+    tokens=[x for x in filter(lambda x: x.isalpha(),text.lower().split())]  
+    sr= stopwords.words('english')
+    #throwing out 'stop words'
+    return [x for x in filter(lambda x: x not in sr,tokens )]
 
 class Article:
     def __init__(self,text):
         self.text=text  
-        #splitting text and filtering words containing only letters
-        tokens=[x for x in filter(lambda x: x.isalpha(),text.lower().split())]  
-        sr= stopwords.words('english')
-        #throwing out 'stop words'
-        self.tokens = [x for x in filter(lambda x: x not in sr,tokens )]
+        self.tokens = txt_to_tokens(text)
         self.freq = FreqDist(self.tokens)
+        
     def print_frequencies (self,n=20):
         for key,value in self.freq.most_common(n):
             print(key,': ',value)
